b9ae78846677f3f7622bccb9580e44901042ec1e
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2025-03-19 21:40:48 +0200

    Update Quiz.js

--- backend/models/Quiz.js
+++ backend/models/Quiz.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 import { handleSaveError, setUpdateSetting } from "./hooks.js";
 
-const quizShema = new Schema(
+const quizSchema = new Schema(
   {
     title: {
       type: String,
@@ -11,21 +11,22 @@ const quizShema = new Schema(
       type: String,
       required: true,
     },
-    questions: {
-      question: { type: String, required: true },
-      type: { type: String, required: true },
-      options: { type: [String], required: true },
-      correctAnswers: { type: [String], required: true },
-    },
+    questions: [
+      {
+        question: { type: String, required: true },
+        type: { type: String, required: true },
+        options: { type: [String], required: true },
+        correctAnswers: { type: [String], required: true },
+      },
+    ],
     statistics: {
       completions: { type: Number, default: 0 },
     },
   },
   { versionKey: false, timestamps: true }
 );
-quizShema.post("save", handleSaveError);
-quizShema.pre("findOneAndUpdate", setUpdateSetting);
-quizShema.post("findOneAndUpdate", handleSaveError);
+
+
 const Quiz = model("quiz", quizShema);
 
 export default Quiz;
